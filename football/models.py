f1da4df02ed40eef52a447a3b7451b285efe25a4
Brighht <bright.andohh@gmail.com>
2025-03-02 21:32:14 -0600

    player-model-init

--- football/models.py
+++ football/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Players(models.Model):
+    player_name = models.CharField(max_length=100)
+    player_jersey_number = models.IntegerField()
+    player_nationality = models.CharField(max_length=100)
+    player_age = models.IntegerField()
+    team_name  = models.CharField(max_length=100)
+    dateofbirth = models.DateField()
+    date_joined = models.DateField()
