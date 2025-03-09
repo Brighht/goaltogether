21b66f94014378bba2e42669e9f84053a1091c74
Brighht <bright.andohh@gmail.com>
2025-03-09 15:58:59 -0500

    register-models in admin.py

--- football/admin.py
+++ football/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Messages, Players, Teams, Responses
 # Register your models here.
+admin.site.register(Messages)
+admin.site.register(Responses)
+admin.site.register(Players)
+admin.site.register(Teams)
